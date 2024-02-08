4a5b425bca1db5e4da48dc9af413118cb73b8e64
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-02-08 16:20:50 +0530

    Updated account routes

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -16,6 +16,43 @@ router.get("/balance", authMiddleware, async (req, res) => {
 });
 
 
+router.post("/transfer", authMiddleware, async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { to_id, amount } = req.body;
+    const fromUsername = req.username;
+    const fromUser = await User.findOne({ username: fromUsername });
+    const fromUserid = fromUser._id;
+
+    const fromAccount = await Account.findOne({ userId: fromUserid }).session(session);
+    if (!fromAccount || fromAccount.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Insufficient balance" });
+    };
+
+    const toAccount = await Account.findOne({ userId: to_id });
+    if (!toAccount) {
+      res.status(400).json({ message: "Incorrect account" });
+    };
+
+    await Account.updateOne({ userId: fromUserid }, {
+      $inc: { balance: -amount }
+    }).session(session);
+    await Account.updateOne({ userId: to_id }, {
+      $inc: { balance: amount }
+    });
+
+    await session.commitTransaction();
+    res.status(200).json({ message: "Transfer successfull" });
+
+  } catch (error) {
+    await session.abortTransaction();
+    res.status(400).json({ message: "Something went wrong" });
+  }
+})
+
 
 
 
